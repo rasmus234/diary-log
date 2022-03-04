9bb4cd9b4bb5da6e576e64c633748b9e80f23a1f
rasmus <rasmussonntag@gmail.com>
2022-03-04 14:19:38 +0100

    fix automapper

--- DiaryLog/DiaryLogApi/Program.cs
+++ DiaryLog/DiaryLogApi/Program.cs
@@ -1,3 +1,4 @@
+using DiaryLogApi;
 using DiaryLogDomain;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +10,8 @@ builder.Services.AddDbContext<DiaryLogContext>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAutoMapper(typeof(MappingProfile));
+
 
 var app = builder.Build();
 
