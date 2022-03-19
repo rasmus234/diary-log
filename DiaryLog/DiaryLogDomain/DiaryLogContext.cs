9768352046476f179dca437ec0faac91127691a7
CKDenni <deni3278@easv.dk>
2022-03-20 00:08:15 +0100

    Fix connection string

--- DiaryLog/DiaryLogDomain/DiaryLogContext.cs
+++ DiaryLog/DiaryLogDomain/DiaryLogContext.cs
@@ -25,8 +25,7 @@ public partial class DiaryLogContext : DbContext
     {
         if (!optionsBuilder.IsConfigured)
         {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-            optionsBuilder.UseSqlServer("Provider=SQLOLEDB.1;Persist Security Info=True;User ID=sa;Initial Catalog=DiaryLog;Data Source=20.123.8.20,8092");
+            optionsBuilder.UseSqlServer("Server=20.123.8.20,8092;Database=DiaryLog;User Id=sa;Password=Stigmastigma1;");
         }
     }
 
